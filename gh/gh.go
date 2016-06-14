f46b4fe2635769f67c639c6c7fbdc88dcec813cf
mh-cbon <mh-cbon@users.noreply.github.com>
2016-06-14 18:46:40 +0200

    go fmt

--- gh/gh.go
+++ gh/gh.go
@@ -1,119 +1,119 @@
 package gh
 
 import (
-	"time"
+	"errors"
 	"regexp"
 	"strings"
-	"errors"
+	"time"
 
 	"github.com/google/go-github/github"
 )
 
-func Add (username string, password string, OTP string, name string, permissions []string) (*github.Authorization, error) {
+func Add(username string, password string, OTP string, name string, permissions []string) (*github.Authorization, error) {
 
-  tp := github.BasicAuthTransport{
-    Username: strings.TrimSpace(username),
-    Password: strings.TrimSpace(password),
-    OTP:      strings.TrimSpace(OTP),
-  }
+	tp := github.BasicAuthTransport{
+		Username: strings.TrimSpace(username),
+		Password: strings.TrimSpace(password),
+		OTP:      strings.TrimSpace(OTP),
+	}
 
-  client := github.NewClient(tp.Client())
+	client := github.NewClient(tp.Client())
 
-  authReq, notFound := GeneratePersonalAuthTokenRequest(name, permissions)
-  if len(notFound)>0 {
-    return nil, errors.New("Unknown permissions: " + strings.Join(notFound, ","))
-  }
+	authReq, notFound := GeneratePersonalAuthTokenRequest(name, permissions)
+	if len(notFound) > 0 {
+		return nil, errors.New("Unknown permissions: " + strings.Join(notFound, ","))
+	}
 
-  createdAuth, _, err := client.Authorizations.Create(authReq)
+	createdAuth, _, err := client.Authorizations.Create(authReq)
 
-  return createdAuth, err
+	return createdAuth, err
 }
 
-func List (username string, password string, OTP string) (map[string]*github.Authorization, error) {
+func List(username string, password string, OTP string) (map[string]*github.Authorization, error) {
 
-  ret := make(map[string]*github.Authorization)
+	ret := make(map[string]*github.Authorization)
 
-  tp := github.BasicAuthTransport{
-    Username: strings.TrimSpace(username),
-    Password: strings.TrimSpace(password),
-    OTP:      strings.TrimSpace(OTP),
-  }
+	tp := github.BasicAuthTransport{
+		Username: strings.TrimSpace(username),
+		Password: strings.TrimSpace(password),
+		OTP:      strings.TrimSpace(OTP),
+	}
 
-  client := github.NewClient(tp.Client())
+	client := github.NewClient(tp.Client())
 
 	opt := &github.ListOptions{Page: 1, PerPage: 200}
 	got, _, err := client.Authorizations.List(opt)
 
-  namedAuth := regexp.MustCompile(`^([^:]+): generated via gh-auth`)
-  for _, v := range got {
-    note := ""
-    if v.Note!=nil {
-      note = *v.Note
-    }
-    if namedAuth.MatchString(note) {
-      parts := namedAuth.FindAllStringSubmatch(note, -1)
-      name := parts[0][1]
-      ret[name] = &v
-    }
-  }
+	namedAuth := regexp.MustCompile(`^([^:]+): generated via gh-auth`)
+	for _, v := range got {
+		note := ""
+		if v.Note != nil {
+			note = *v.Note
+		}
+		if namedAuth.MatchString(note) {
+			parts := namedAuth.FindAllStringSubmatch(note, -1)
+			name := parts[0][1]
+			ret[name] = &v
+		}
+	}
 
-  return ret, err
+	return ret, err
 }
 
-func Delete (username string, password string, OTP string, id int) error {
+func Delete(username string, password string, OTP string, id int) error {
 
-  tp := github.BasicAuthTransport{
-    Username: strings.TrimSpace(username),
-    Password: strings.TrimSpace(password),
-    OTP:      strings.TrimSpace(OTP),
-  }
+	tp := github.BasicAuthTransport{
+		Username: strings.TrimSpace(username),
+		Password: strings.TrimSpace(password),
+		OTP:      strings.TrimSpace(OTP),
+	}
 
-  client := github.NewClient(tp.Client())
+	client := github.NewClient(tp.Client())
 
 	_, err := client.Authorizations.Delete(id)
 
-  return err
+	return err
 }
 
 //
 func GeneratePersonalAuthTokenRequest(name string, permissions []string) (*github.AuthorizationRequest, []string) {
-  scopes := map[string]github.Scope{
-    "user":               github.ScopeUser,
-    "user:email":         github.ScopeUserEmail,
-    "user:follow":        github.ScopeUserFollow,
-    "public_repo":        github.ScopePublicRepo,
-    "repo":               github.ScopeRepo,
-    "repo_deployment":    github.ScopeRepoDeployment,
-    "repo:status":        github.ScopeRepoStatus,
-    "delete_repo":        github.ScopeDeleteRepo,
-    "notifications":      github.ScopeNotifications,
-    "gist":               github.ScopeGist,
-    "read:repo_hook":     github.ScopeReadRepoHook,
-    "write:repo_hook":    github.ScopeWriteRepoHook,
-    "admin:repo_hook":    github.ScopeAdminRepoHook,
-    "admin:org_hook":     github.ScopeAdminOrgHook,
-    "read:org":           github.ScopeReadOrg,
-    "write:org":          github.ScopeWriteOrg,
-    "admin:org":          github.ScopeAdminOrg,
-    "read:public_key":    github.ScopeReadPublicKey,
-    "write:public_key":   github.ScopeWritePublicKey,
-    "admin:public_key":   github.ScopeAdminPublicKey,
-    "read:gpg_key":       github.ScopeReadGPGKey,
-    "write:gpg_key":      github.ScopeWriteGPGKey,
-    "admin:gpg_key":      github.ScopeAdminGPGKey,
-  }
-  notFound := make([]string, 0)
+	scopes := map[string]github.Scope{
+		"user":             github.ScopeUser,
+		"user:email":       github.ScopeUserEmail,
+		"user:follow":      github.ScopeUserFollow,
+		"public_repo":      github.ScopePublicRepo,
+		"repo":             github.ScopeRepo,
+		"repo_deployment":  github.ScopeRepoDeployment,
+		"repo:status":      github.ScopeRepoStatus,
+		"delete_repo":      github.ScopeDeleteRepo,
+		"notifications":    github.ScopeNotifications,
+		"gist":             github.ScopeGist,
+		"read:repo_hook":   github.ScopeReadRepoHook,
+		"write:repo_hook":  github.ScopeWriteRepoHook,
+		"admin:repo_hook":  github.ScopeAdminRepoHook,
+		"admin:org_hook":   github.ScopeAdminOrgHook,
+		"read:org":         github.ScopeReadOrg,
+		"write:org":        github.ScopeWriteOrg,
+		"admin:org":        github.ScopeAdminOrg,
+		"read:public_key":  github.ScopeReadPublicKey,
+		"write:public_key": github.ScopeWritePublicKey,
+		"admin:public_key": github.ScopeAdminPublicKey,
+		"read:gpg_key":     github.ScopeReadGPGKey,
+		"write:gpg_key":    github.ScopeWriteGPGKey,
+		"admin:gpg_key":    github.ScopeAdminGPGKey,
+	}
+	notFound := make([]string, 0)
 	auth := github.AuthorizationRequest{
-		Note:        github.String(name+": generated via gh-auth"),
+		Note:        github.String(name + ": generated via gh-auth"),
 		Scopes:      []github.Scope{},
 		Fingerprint: github.String(name + time.Now().String()),
 	}
-  for _, p := range permissions {
-    if val, ok := scopes[p]; ok {
-      auth.Scopes = append(auth.Scopes, val)
-    } else {
-      notFound = append(notFound, p)
-    }
-  }
+	for _, p := range permissions {
+		if val, ok := scopes[p]; ok {
+			auth.Scopes = append(auth.Scopes, val)
+		} else {
+			notFound = append(notFound, p)
+		}
+	}
 	return &auth, notFound
 }
--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
 	"fmt"
 	"os"
-	"bufio"
-	"syscall"
 	"strings"
-	"encoding/json"
+	"syscall"
 
 	"github.com/google/go-github/github"
 	"github.com/mh-cbon/gh-api-cli/GenVersionFile"
@@ -23,284 +23,284 @@ func main() {
 	app.Version = GenVersionFile.Version()
 	app.Usage = "Github api command line client"
 	app.UsageText = "gh-api-cli <cmd> <options>"
-  app.Commands = []cli.Command{
-    {
-      Name:    "add-auth",
-      Usage:   "Add a new authorization",
-      Action:  add,
-      Flags: []cli.Flag {
-        cli.StringFlag{
-          Name: "username, u",
-          Value: "",
-          Usage: "Github username",
-        },
-        cli.StringFlag{
-          Name: "password, p",
-          Value: "",
-          Usage: "Github password",
-        },
-        cli.StringFlag{
-          Name: "name, n",
-          Value: "",
-          Usage: "Name of the authorization",
-        },
-        cli.StringSliceFlag{
-    			Name:  "rights, r",
-    			Value: &cli.StringSlice{},
-    			Usage: "Permissions to set",
-        },
-      },
-    },
-    {
-      Name:    "list-auth",
-      Usage:   "List authorizations",
-      Action:  list,
-      Flags: []cli.Flag {
-        cli.StringFlag{
-          Name: "username, u",
-          Value: "",
-          Usage: "Github username",
-        },
-        cli.StringFlag{
-          Name: "password, p",
-          Value: "",
-          Usage: "Github password",
-        },
-      },
-    },
-    {
-      Name:    "rm-auth",
-      Usage:   "Remove an authorization",
-      Action:  rm,
-      Flags: []cli.Flag {
-        cli.StringFlag{
-          Name: "username, u",
-          Value: "",
-          Usage: "Github username",
-        },
-        cli.StringFlag{
-          Name: "password, p",
-          Value: "",
-          Usage: "Github password",
-        },
-        cli.StringFlag{
-          Name: "name, n",
-          Value: "",
-          Usage: "Name of the authorization",
-        },
-      },
-    },
-    {
-      Name:    "get-auth",
-      Usage:   "Get token from a locally saved authorization",
-      Action:  get,
-      Flags: []cli.Flag {
-        cli.StringFlag{
-          Name: "name, n",
-          Value: "",
-          Usage: "Name of the authorization",
-        },
-      },
-    },
-  }
+	app.Commands = []cli.Command{
+		{
+			Name:   "add-auth",
+			Usage:  "Add a new authorization",
+			Action: add,
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "username, u",
+					Value: "",
+					Usage: "Github username",
+				},
+				cli.StringFlag{
+					Name:  "password, p",
+					Value: "",
+					Usage: "Github password",
+				},
+				cli.StringFlag{
+					Name:  "name, n",
+					Value: "",
+					Usage: "Name of the authorization",
+				},
+				cli.StringSliceFlag{
+					Name:  "rights, r",
+					Value: &cli.StringSlice{},
+					Usage: "Permissions to set",
+				},
+			},
+		},
+		{
+			Name:   "list-auth",
+			Usage:  "List authorizations",
+			Action: list,
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "username, u",
+					Value: "",
+					Usage: "Github username",
+				},
+				cli.StringFlag{
+					Name:  "password, p",
+					Value: "",
+					Usage: "Github password",
+				},
+			},
+		},
+		{
+			Name:   "rm-auth",
+			Usage:  "Remove an authorization",
+			Action: rm,
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "username, u",
+					Value: "",
+					Usage: "Github username",
+				},
+				cli.StringFlag{
+					Name:  "password, p",
+					Value: "",
+					Usage: "Github password",
+				},
+				cli.StringFlag{
+					Name:  "name, n",
+					Value: "",
+					Usage: "Name of the authorization",
+				},
+			},
+		},
+		{
+			Name:   "get-auth",
+			Usage:  "Get token from a locally saved authorization",
+			Action: get,
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "name, n",
+					Value: "",
+					Usage: "Name of the authorization",
+				},
+			},
+		},
+	}
 
 	app.Run(os.Args)
 }
 
-func add (c *cli.Context) error {
+func add(c *cli.Context) error {
 
-  name  := c.String("name")
-  perms := make([]string, 0)
-  for _, p := range c.StringSlice("rights") {
-    perms = append(perms, string(p))
-  }
-
-  if len(name)==0 {
-    return cli.NewExitError("You must provide name", 1)
-  }
-  if len(perms)==0 {
-    return cli.NewExitError("You must provide permissions", 1)
-  }
-
-  username  := getUsername(c.String("username"))
-  password  := getPassword(c.String("password"))
-
-  auths, err := gh.List(username, password, "")
-	if _, ok := err.(*github.TwoFactorAuthError); err != nil && ok {
-		otp := queryOtp()
-    auths, err = gh.List(username, password, otp)
-    if err!=nil {
-      fmt.Println(err)
-      return cli.NewExitError("Could not list current authorizations!", 1)
-    }
+	name := c.String("name")
+	perms := make([]string, 0)
+	for _, p := range c.StringSlice("rights") {
+		perms = append(perms, string(p))
 	}
 
-  //-auths
-  if _, ok := auths[name]; ok {
-    return cli.NewExitError("Authorization " + name + " already exists!", 1)
-  }
-
-  createdAuth, err := gh.Add(username, password, "", name, perms)
-	if _, ok := err.(*github.TwoFactorAuthError); err != nil && ok {
-		otp := queryOtp()
-    createdAuth, err = gh.Add(username, password, otp, name, perms)
+	if len(name) == 0 {
+		return cli.NewExitError("You must provide name", 1)
+	}
+	if len(perms) == 0 {
+		return cli.NewExitError("You must provide permissions", 1)
 	}
 
-  if err!=nil {
-    fmt.Println(err)
-    return cli.NewExitError("The request could not be completed!", 1)
-  }
+	username := getUsername(c.String("username"))
+	password := getPassword(c.String("password"))
 
-  err = local.Add(name, createdAuth)
-  if err!=nil {
-    fmt.Println(err)
-    fmt.Println("The new token was not saved on your local!")
-  }
+	auths, err := gh.List(username, password, "")
+	if _, ok := err.(*github.TwoFactorAuthError); err != nil && ok {
+		otp := queryOtp()
+		auths, err = gh.List(username, password, otp)
+		if err != nil {
+			fmt.Println(err)
+			return cli.NewExitError("Could not list current authorizations!", 1)
+		}
+	}
 
-  fmt.Println("New authorization '"+name+"' created!")
-  fmt.Println(string(*createdAuth.Token))
+	//-auths
+	if _, ok := auths[name]; ok {
+		return cli.NewExitError("Authorization "+name+" already exists!", 1)
+	}
 
-  return nil
+	createdAuth, err := gh.Add(username, password, "", name, perms)
+	if _, ok := err.(*github.TwoFactorAuthError); err != nil && ok {
+		otp := queryOtp()
+		createdAuth, err = gh.Add(username, password, otp, name, perms)
+	}
+
+	if err != nil {
+		fmt.Println(err)
+		return cli.NewExitError("The request could not be completed!", 1)
+	}
+
+	err = local.Add(name, createdAuth)
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("The new token was not saved on your local!")
+	}
+
+	fmt.Println("New authorization '" + name + "' created!")
+	fmt.Println(string(*createdAuth.Token))
+
+	return nil
 }
 
-func list (c *cli.Context) error {
+func list(c *cli.Context) error {
 
-  username  := getUsername(c.String("username"))
-  password  := getPassword(c.String("password"))
+	username := getUsername(c.String("username"))
+	password := getPassword(c.String("password"))
 
-  auths, err := gh.List(username, password, "")
+	auths, err := gh.List(username, password, "")
 	if _, ok := err.(*github.TwoFactorAuthError); err != nil && ok {
 		otp := queryOtp()
-    auths, err = gh.List(username, password, otp)
+		auths, err = gh.List(username, password, otp)
 	}
-  if err!=nil {
-    return cli.NewExitError("Could not list current authorizations!", 1)
-  }
+	if err != nil {
+		return cli.NewExitError("Could not list current authorizations!", 1)
+	}
 
-  saved := local.Read()
+	saved := local.Read()
 
-  for name, auth := range auths {
-    if val, ok := saved[name]; ok {
-      if val.Token!=nil {
-        auth.Token = github.String(*val.Token)
-      } else {
-        auth.Token = github.String("Unknown on your local")
-      }
-    } else {
-      auth.Token = github.String("Unknown on your local")
-    }
-  }
+	for name, auth := range auths {
+		if val, ok := saved[name]; ok {
+			if val.Token != nil {
+				auth.Token = github.String(*val.Token)
+			} else {
+				auth.Token = github.String("Unknown on your local")
+			}
+		} else {
+			auth.Token = github.String("Unknown on your local")
+		}
+	}
 
-  jsonContent, err := jsonString(auths)
-  if err!=nil {
-    fmt.Println(err)
-    return cli.NewExitError("There was an error while printing your results!", 1)
-  }
-  fmt.Println(jsonContent)
+	jsonContent, err := jsonString(auths)
+	if err != nil {
+		fmt.Println(err)
+		return cli.NewExitError("There was an error while printing your results!", 1)
+	}
+	fmt.Println(jsonContent)
 
-  return nil
+	return nil
 }
 
-func rm (c *cli.Context) error {
-  name  := c.String("name")
+func rm(c *cli.Context) error {
+	name := c.String("name")
 
-  if len(name)==0 {
-    return cli.NewExitError("You must provide a name", 1)
-  }
+	if len(name) == 0 {
+		return cli.NewExitError("You must provide a name", 1)
+	}
 
-  otp := ""
-  username  := getUsername(c.String("username"))
-  password  := getPassword(c.String("password"))
+	otp := ""
+	username := getUsername(c.String("username"))
+	password := getPassword(c.String("password"))
 
-  auths, err := gh.List(username, password, "")
+	auths, err := gh.List(username, password, "")
 	if _, ok := err.(*github.TwoFactorAuthError); err != nil && ok {
 		otp = queryOtp()
-    auths, err = gh.List(username, password, otp)
+		auths, err = gh.List(username, password, otp)
 	}
-  if err!=nil {
-    fmt.Println(err)
-    return cli.NewExitError("Could not list current authorizations!", 1)
-  }
+	if err != nil {
+		fmt.Println(err)
+		return cli.NewExitError("Could not list current authorizations!", 1)
+	}
 
-  if val, ok := auths[name]; ok {
-    err = gh.Delete(username, password, otp, *val.ID)
-    if err!=nil {
-      fmt.Println(err)
-      return cli.NewExitError("The deletion failed!", 1)
-    }
-  } else {
-    return cli.NewExitError("Authorization " + name + " was not found!", 1)
-  }
+	if val, ok := auths[name]; ok {
+		err = gh.Delete(username, password, otp, *val.ID)
+		if err != nil {
+			fmt.Println(err)
+			return cli.NewExitError("The deletion failed!", 1)
+		}
+	} else {
+		return cli.NewExitError("Authorization "+name+" was not found!", 1)
+	}
 
-  err = local.Remove(name)
-  if err!=nil {
-    fmt.Println(err)
-    fmt.Println("The authorization was not removed on your local!")
-  }
+	err = local.Remove(name)
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("The authorization was not removed on your local!")
+	}
 
-  fmt.Println("Deleted authorization: " + name)
+	fmt.Println("Deleted authorization: " + name)
 
-  return nil
+	return nil
 }
 
-func get (c *cli.Context) error {
-  name  := c.String("name")
+func get(c *cli.Context) error {
+	name := c.String("name")
 
-  if len(name)==0 {
-    return cli.NewExitError("You must provide a name", 1)
-  }
+	if len(name) == 0 {
+		return cli.NewExitError("You must provide a name", 1)
+	}
 
-  auth, err := local.Get(name)
-  if err!=nil {
-    fmt.Println(err)
-    return cli.NewExitError("The authorization '" + name + "' was not found on your local!", 1)
-  }
+	auth, err := local.Get(name)
+	if err != nil {
+		fmt.Println(err)
+		return cli.NewExitError("The authorization '"+name+"' was not found on your local!", 1)
+	}
 
-  if auth.Token==nil {
-    return cli.NewExitError("The authorization '" + name + "' does not have token!", 1)
-  }
-  fmt.Println(*auth.Token)
+	if auth.Token == nil {
+		return cli.NewExitError("The authorization '"+name+"' does not have token!", 1)
+	}
+	fmt.Println(*auth.Token)
 
-  return nil
+	return nil
 }
 
-func getUsername( username string) string {
-  if strings.TrimSpace(username)=="" {
-    username = queryUsername()
-  }
-  return username
+func getUsername(username string) string {
+	if strings.TrimSpace(username) == "" {
+		username = queryUsername()
+	}
+	return username
 }
 func getPassword(password string) string {
-  if strings.TrimSpace(password)=="" {
-    password = queryPassword()
-  }
-  return password
+	if strings.TrimSpace(password) == "" {
+		password = queryPassword()
+	}
+	return password
 }
 
-func queryUsername () string {
-  r := bufio.NewReader(os.Stdin)
+func queryUsername() string {
+	r := bufio.NewReader(os.Stdin)
 	fmt.Print("GitHub Username: ")
 	username, _ := r.ReadString('\n')
-  return strings.TrimSpace(username)
+	return strings.TrimSpace(username)
 }
 
-func queryOtp () string {
-  r := bufio.NewReader(os.Stdin)
-  fmt.Print("\nGitHub One Time Password: ")
-  otp, _ := r.ReadString('\n')
-  return strings.TrimSpace(otp)
+func queryOtp() string {
+	r := bufio.NewReader(os.Stdin)
+	fmt.Print("\nGitHub One Time Password: ")
+	otp, _ := r.ReadString('\n')
+	return strings.TrimSpace(otp)
 }
 
-func queryPassword () string {
+func queryPassword() string {
 	fmt.Print("GitHub Password: ")
 	bytePassword, _ := terminal.ReadPassword(int(syscall.Stdin))
-  fmt.Println("")
+	fmt.Println("")
 	return string(bytePassword)
 }
 
-func jsonString (some interface{}) (string, error) {
-  jsonContent, err := json.MarshalIndent(some, "", "    ")
-  return string(jsonContent), err
+func jsonString(some interface{}) (string, error) {
+	jsonContent, err := json.MarshalIndent(some, "", "    ")
+	return string(jsonContent), err
 }
 
 func exitWithError(err error) {
